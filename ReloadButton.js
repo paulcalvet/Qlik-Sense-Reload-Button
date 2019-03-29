ea0e794da8848bc7a446aa940a8706620a53fec1
paulcalvet <paulcalvet@yahoo.fr>
2019-03-29 10:58:28 +0100

    Connexion SSE + Modification Extension

--- ReloadButton.js
+++ ReloadButton.js
@@ -1,21 +1,157 @@
 define(["jquery", "qlik", "text!./lib/css/reload_btn.css"], function($, qlik, cssContent) {
-  $("<style>").html(cssContent).appendTo("head");
+
+    var header0_item1 = {
+    ref: "props.section0.item1",
+    expression: "optional",
+    label: "Host name",
+    type: "string"
+    };
+
+    var header0_item2 = {
+    ref: "props.section0.item2",
+    expression: "optional",
+    label: "Port",
+    type: "string"
+    };
+
+    var header0_item3 = {
+    ref: "props.section0.item3",
+    expression: "optional",
+    label: "Owner",
+    type: "string"
+    };
+
+    var header1_item1 = {
+    ref: "props.section1.item1",
+    expression: "optional",
+    label: "SSE",
+    type: "string"
+    };
+
+    var header2_item1 = {
+    ref: "props.section2.item1",
+    expression: "optional",
+    label: "Confirmation message",
+    type: "string"
+    };
+
+    var header2_item2 = {
+    ref: "props.section2.item2",
+    expression: "optional",
+    label: "Success message",
+    type: "string"
+    };
+
+    var header2_item3 = {
+    ref: "props.section2.item3",
+    expression: "optional",
+    label: "Failed message",
+    type: "string"
+    };
+
+    var header3_item1 = {
+    ref: "myproperties.border",
+    type: "boolean",
+    component: "switch",
+    label: "Reload type",
+    options: [{
+        value: true,
+        label: "Partial Reload"
+        }, {
+        value: false,
+        label: "Full Reload"
+        }],
+    defaultValue: true
+    };
+
+
+    var header4_item1 = {
+    ref: "props.section4.item1",
+    expression: "optional",
+    label: "Button text",
+    type: "string"
+    };
+
+    $("<style>").html(cssContent).appendTo("head");
 
 	return {
-		paint: function ($element, layout) {
-      var app = qlik.currApp(this);
 
-		  //Check if Qlik Sense Desktop or Server
-		  var isPersonalMode = true;
+        definition: {
+            type: "items",
+            component: "accordion",
+            items: {
 
-			var global = qlik.getGlobal();
-			global.isPersonalMode( function ( reply ) {
-				isPersonalMode = reply.qReturn;
-			});
+            appearance: {
+                uses: "settings"
+            },
 
-		  // Display Extension Visualization
-      var html = html = '<a href="#" id="modal-open" class="btn btn-primary">Reload</a>';
-      $element.html( html );
+            addons: {
+				uses: "addons",
+
+                    items: {
+                        header0: {
+                            type: "items",
+                            label: "Query parameters",
+                            items: {
+                                header0_item1: header0_item1,
+                                header0_item2: header0_item2,
+                                header0_item3: header0_item3
+                                }
+                            },
+                        header1: {
+                            type: "items",
+                            label: "SSE Function",
+                            items: {
+                                header1_item1: header1_item1
+                                }
+                            },
+                        header2: {
+                            type: "items",
+                            label: "Message",
+                            items: {
+                                header2_item1: header2_item1,
+                                header2_item2: header2_item2,
+                                header2_item3: header2_item3
+                                }
+                            },
+                        header3: {
+                            type: "items",
+                            label: "Reload Option",
+                            items: {
+                                header3_item1:header3_item1
+
+                            }
+                        },
+                        header4: {
+                            type: "items",
+                            label: "Button Option",
+                            items: {
+                                header4_item1:header4_item1
+
+                            }
+                        }
+
+                    }
+                }
+            }
+        },
+
+
+
+        paint: function ($element, layout) {
+        var app = qlik.currApp(this);
+
+		//Check if Qlik Sense Desktop or Server
+		var isPersonalMode = true;
+
+		var global = qlik.getGlobal();
+		global.isPersonalMode( function ( reply ) {
+		  isPersonalMode = reply.qReturn;
+		});
+
+        // Display Extension Visualization
+        var html = html = '<a href="#" id="modal-open" class="btn btn-primary">'+layout.props.section4.item1+'</a>';
+        $element.html( html );
 
 			// Open modal
 			$("#modal-open").click(function(event) {
@@ -23,35 +159,37 @@ define(["jquery", "qlik", "text!./lib/css/reload_btn.css"], function($, qlik, cs
 
 				$(this).blur() ;
 
-				// Check if modal is displayed
-				if($("#modal-overlay")[0]) return false ;
+                    // Check if modal is displayed
+                    if($("#modal-overlay")[0]) return false ;
 
-				// Add modal overlay
-				$(".qv-panel-sheet").append('<div id="modal-overlay"></div>');
-				$("#modal-overlay").fadeIn("slow");
+                    // Add modal overlay
+                    $(".qv-panel-sheet").append('<div id="modal-overlay"></div>');
+                    $("#modal-overlay").fadeIn("slow");
 
-				// Add modal panel
-				$(".qv-panel-sheet").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Are you sure to execute reload?</h2></div><div id="modal-checkbox"><input type="checkbox" id="partial" name="partial" value=""><label for="partial">Partial reload</label></div><div id="modal-botton"><a href="#" id="execute-reload" class="btn btn-primary"> OK </a><a href="#" id="modal-close" class="btn btn-danger">Cancel</a></div></div>');
-				$("#modal-content").fadeIn("slow");
+                    // Add modal panel
+                    $(".qv-panel-sheet").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>'+layout.props.section2.item1+'</h2></div><div id="modal-botton"><a href="#" id="execute-reload" class="btn btn-primary"> OK </a><a href="#" id="modal-close" class="btn btn-danger">Cancel</a></div></div>');
+                    $("#modal-content").fadeIn("slow");
 
-				// Close modal
-				$("#modal-overlay, #modal-close").unbind().click(function(event) {
-					event.preventDefault();
-					$("#modal-content,#modal-overlay").fadeOut("slow", function() {
-						$("#modal-content").remove();
-						$("#modal-overlay").remove();
-					});
-				});
+                    // Close modal
+                    $("#modal-overlay, #modal-close").unbind().click(function(event) {
+                        event.preventDefault();
+                        $("#modal-content,#modal-overlay").fadeOut("slow", function() {
+                            $("#modal-content").remove();
+                            $("#modal-overlay").remove();
+                        });
+                    });
+
 
 				// Execute reload
 				$("#execute-reload").click(function(event) {
-					event.preventDefault();
+
+                    event.preventDefault();
 
 					// Check if reload is partial
-					var isPartial = false;
-					if($("#partial").prop('checked')) {
-						isPartial = true;
-					}
+					var isPartial = layout.myproperties.border;
+					//if($("#partial").prop('checked')) {
+					//	isPartial = true;
+					//}
 
 					// Remove modal
 					$("#modal-content").remove();
@@ -59,37 +197,73 @@ define(["jquery", "qlik", "text!./lib/css/reload_btn.css"], function($, qlik, cs
 					// Open loader circle
 					$("#modal-overlay").append('<div id="loader" class="loader">Loading...</div>');
 
-					//console.log("isPersonalMode: " + isPersonalMode);
-					//console.log("isPartial: " + isPartial);
 
-					//Execute reload
-					if (isPersonalMode) {
-						app.doReload( 0, isPartial, false).then(function(e) {
-							$("#loader").remove();
-							if(e) {
-								app.doSave();
-								$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload succeeded!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-success">Close</a></div></div>');
-							} else {
-								$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload failed!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-danger">Close</a></div></div>');
-							}
-							$("#modal-content").fadeIn("slow");
-						});
-					} else {
-						// qlik.callRepository( '/qrs/app/' + appid + '/reload', 'POST' ).success( function ( reply ) {
-						//	$("#modal-overlay").remove()
-						// } );
-						app.doReload( 0, isPartial, false).then(function(e) {
-							$("#loader").remove();
-							if(e) {
-								app.doSave();
-								$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload succeeded!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-success">Close</a></div></div>');
-							} else {
-								$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload failed!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-danger">Close</a></div></div>');
-							}
-							$("#modal-content").fadeIn("slow");
-						});
-					}
-				});
+
+                    app.createCube({
+                    "qInitialDataFetch": [
+                        {
+                            "qHeight": 20,
+                            "qWidth": 1
+                        }
+                    ],
+                    "qDimensions": [],
+                    "qMeasures": [
+                        {
+                            "qDef": {
+                                "qDef": layout.props.section1.item1
+                            },
+                            "qLabel": "GetApplication",
+                            "qLibraryId": null,
+                            "qSortBy": {
+                                "qSortByState": 0,
+                                "qSortByFrequency": 0,
+                                "qSortByNumeric": 0,
+                                "qSortByAscii": 1,
+                                "qSortByLoadOrder": 0,
+                                "qSortByExpression": 0,
+                                "qExpression": {
+                                    "qv": " "
+                                }
+                            }
+                        }
+                    ],
+                    "qSuppressZero": false,
+                    "qSuppressMissing": false,
+                    "qMode": "S",
+                    "qInterColumnSortOrder": [],
+                    "qStateName": "$"
+                    },GetApplication);
+
+
+                    function GetApplication(reply, app){
+
+                        var output = reply.qHyperCube.qDataPages[0].qMatrix[0][0].qText;
+                        app.destroySessionObject(reply.qInfo.qId);
+                        // Condition OK, on recharge les données dasn Qlik
+                        if (output == 0) {
+                            app.doReload( 0, isPartial, false).then(function(e) {
+                                $("#loader").remove();
+                                if(e) {
+                                    app.doSave();
+
+                                    $("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>'+layout.props.section2.item2+'</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-success">Close</a></div></div>');
+                                } else {
+
+                                    $("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>'+layout.props.section2.item3+'</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-danger">Close</a></div></div>');
+                                }
+                                $("#modal-content").fadeIn("slow");
+                            })
+
+                        // Condition KO, on ne charge pas les données dans Qlik
+                        } else {
+
+                            $("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload failed!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-danger">Close</a></div></div>');
+                            }
+                            $("#modal-content").fadeIn("slow");
+	                    };
+
+				}); // End Execute reload
+
 			});
 		}
 	};
